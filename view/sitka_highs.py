5e07cca969c278b0d958bc062fa567df4eaefc66
1kuzu <1145956013@qq.com>
2024-07-05 02:33:15 +0800

    第一次提交温度绘图

--- view/sitka_highs.py
+++ view/sitka_highs.py
@@ -11,18 +11,39 @@ with open (filename) as f:
     for index,column_header in enumerate(header_row):
         print(index,column_header)
     # 提取并且读取数据
-    highs = [int(row[5]) for row in reader]
+    dates,highs = [],[]
+    for row in reader:
+        current_date = datetime.strptime(row[2],'%Y-%m-%d')
+        high = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+        
+
+
+    #列表解析的形式
+    #reader读取文件过后将会关闭文件，所以需要先提取数据写入列表
+    '''yet_read =[]
+    for line in reader:
+        yet_read.append(line)
+    dates = [datetime.strptime(_[2],'%Y-%m-%d') for _ in yet_read]
+    highs = [int(_[5]) for _ in yet_read]
     print(highs)
+    print(dates)'''
 
 #根据最高温度绘制图形
 plt.style.use('seaborn')
 fig,ax = plt.subplots()
-ax.plot(highs,c='red')
+ax.plot(dates,highs,c='red')
 
 #设置图形的格式
 ax.set_title('The highest degree',fontsize = 24)
 ax.set_xlabel('',fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('degree',fontsize=16)
 ax.tick_params(axis='both',which='major',labelsize=16)
 
 plt.show()
+
+# first_date =datetime.strptime('2018-07-01','%Y-%m-%d')
+# print(first_date)
+
